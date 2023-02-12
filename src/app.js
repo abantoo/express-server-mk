f3473c4abe5f148644a134159036d8a935ffe820
Abanto Naimur <abantonaimur@gmail.com>
2023-02-13 02:16:43 +0600

    New: connection to db

--- src/app.js
+++ src/app.js
@@ -1,4 +1,31 @@
-const express = require("express");
+import chalk from 'chalk';
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import dbConfig from './config/dbConfig.json' assert { type: "json" };
+import serverConfig from './config/serverConfig.json' assert { type: "json" };
+import mongoose from 'mongoose';
+
 const app = express();
-const port = 3000;
-app.listen(port,() => console.log("\x1b[5m",`This app listening at http://localhost:${port}`))
+
+const port = serverConfig.port;
+const uri = `mongodb+srv://${dbConfig.username}:${dbConfig.password}@${dbConfig.url}/Storage?retryWrites=true&w=majority`
+
+app.use(cors());
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
+try {
+  const connection = await mongoose.connect(uri);
+  
+  if (connection) {
+    console.log(connection);
+    app.listen(port, () =>
+      console.log(chalk.redBright("This app listening at"),chalk.whiteBright.bgRedBright.bold(`http://localhost:${port}`))
+    )
+  }
+}
+catch (e) {
+  console.log(e)
+}
