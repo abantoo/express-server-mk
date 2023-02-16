77d16b1759fe95beb45c1d3c80842d59b2fa97dc
Abanto Naimur <abantonaimur@gmail.com>
2023-02-16 22:31:52 +0600

    adds schema fields

--- src/schema/user.js
+++ src/schema/user.js
@@ -1,7 +1,11 @@
-import mongoose from "mongoose"
+import mongoose from"mongoose";
 
 const userSchema = new mongoose.Schema({
     email: {
+        type: String,
+        required: true
+    },
+    username: {
         type: String,
         required: true
     },
@@ -12,7 +16,22 @@ const userSchema = new mongoose.Schema({
     password: {
       type: String,
       required: true
-    }
+    }, 
+    websites: Array,
+    followers: Number,
+    following: Number,
+    postCount: Number,
+    bio: String,
+    
+    followers: [String],
+    following: [String],
+    // Probably link, will decide later
+    avatar: String,
+
+    firstname: String,
+    lastname: String,
+
+    role: String,
 }, {
     timestamps: true
 });
