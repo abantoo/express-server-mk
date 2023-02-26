8614193f9e2714bd0c6378ed65a414fb5f015254
Abanto Naimur <abantonaimur@gmail.com>
2023-02-26 21:36:40 +0600

    updates post model

--- src/model/perk.js
+++ src/model/perk.js
@@ -3,10 +3,15 @@ import perkSchema from "../schema/perk";
 
 const Perk = mongoose.model("Perk", perkSchema);
 
-// export default class PerkModel {
-//   static async addMotivation(postCreationData) {
-//     Perk.create(postCreationData);
-//   }
-// }
+export default class PerkModel {
+  static async addMotivation(postCreationData) {
+    await Perk.create(postCreationData);
+  }
+
+  static async getAllMotivations() {
+    const result = await Perk.find().lean().exec();
+    return result;
+  }
+}
 
 module.exports = Perk;
